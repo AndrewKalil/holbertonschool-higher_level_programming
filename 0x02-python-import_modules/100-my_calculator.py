c05dd46a9d3c59161887f4a5b5f7eb9ddc046b96
AndrewKalil <1541@holbertonschool.com>
2020-04-30 11:56:14 -0500

    first advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-summ = add(a, b)
-# adds a and b
-print("{:d} + {:d} = {:d}".format(a, b, summ))
-diff = sub(a, b)
-# subtracts a and b
-print("{:d} - {:d} = {:d}".format(a, b, diff))
-prod = mul(a, b)
-# multiplies a and b
-print("{:d} * {:d} = {:d}".format(a, b, prod))
-quot = div(a, b)
-# divides a and b
-print("{:d} / {:d} = {:d}".format(a, b, quot))
+from sys import argv
+if __name__ == "__main__":
+    if (len(argv) == 4):
+        a = int(argv[1])
+        b = int(argv[3])
+        summ = add(a, b) # adds a and b
+        diff = sub(a, b) # subtracts a and b
+        prod = mul(a, b) # multiplies a and b
+        quot = div(a, b) # divides a and b
+        if (argv[2] == "+"):
+            print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, summ))
+        elif (argv[2] == "-"):
+            print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, diff))
+        elif (argv[2] == "*"):
+            print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, prod))
+        elif (argv[2] == "/"):
+            print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, quot))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
