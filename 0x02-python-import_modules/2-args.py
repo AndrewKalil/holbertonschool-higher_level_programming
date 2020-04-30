666034787d24e34f0bbf93698d7ae433431314f6
AndrewKalil <1541@holbertonschool.com>
2020-04-30 02:31:57 -0500

    pep8 for task 2 and adding task 4

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     if (len(argv) == 1):
-        print("{:d} arguments.".format(len(argv) -1))
+        print("{:d} arguments.".format(len(argv) - 1))
     elif (len(argv) == 2):
         print("{:d} argument:".format(len(argv) - 1))
     else:
