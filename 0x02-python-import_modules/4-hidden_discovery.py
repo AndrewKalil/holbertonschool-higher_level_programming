54152e386f5a0b8f4edce1d2ba1730a8f0afadea
AndrewKalil <1541@holbertonschool.com>
2020-04-30 03:04:58 -0500

    adding task 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    list_names = dir(hidden_4)
+    for i in range(len(list_names)):
+        if (i[:2] == "__"):
+            continue
+        else:
+            print(i)
