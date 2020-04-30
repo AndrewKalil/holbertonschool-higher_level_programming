3fb755f68d048c18b6f170ebe10525b3694bffc7
AndrewKalil <1541@holbertonschool.com>
2020-04-30 11:16:33 -0500

    adding empty advanced files

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+summ = add(a, b)
+# adds a and b
+print("{:d} + {:d} = {:d}".format(a, b, summ))
+diff = sub(a, b)
+# subtracts a and b
+print("{:d} - {:d} = {:d}".format(a, b, diff))
+prod = mul(a, b)
+# multiplies a and b
+print("{:d} * {:d} = {:d}".format(a, b, prod))
+quot = div(a, b)
+# divides a and b
+print("{:d} / {:d} = {:d}".format(a, b, quot))
