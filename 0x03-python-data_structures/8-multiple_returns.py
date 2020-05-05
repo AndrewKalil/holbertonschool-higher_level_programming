c0ffdd6842a521c06683792e3f94da7973ad0047
AndrewKalil <1541@holbertonschool.com>
2020-05-05 15:32:21 -0500

    added condition if length is > 0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,7 @@
 def multiple_returns(sentence):
     if sentence == "":
         return None
-    tuple_lf = (len(sentence), sentence[0])
-    return tuple_lf
+    length = len(sentence)
+    if length > 0:
+        tuple_lf = (length, sentence[0])
+        return tuple_lf
