823ffae3ba03e9625df26fce768f6290a8f4e4ab
AndrewKalil <1541@holbertonschool.com>
2020-05-05 16:24:31 -0500

    using append to create a list from empty

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new = []
+    for i in my_list:
+        if my_list[i] % 2 == 0:
+            new.append(True)
+        elif my_list[i] % 2 != 0:
+            new.append(False)
+    return new
