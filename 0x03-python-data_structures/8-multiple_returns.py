7948cae0e1432e4c8ec5d1214eb2d01b09ceef59
AndrewKalil <1541@holbertonschool.com>
2020-05-05 15:34:14 -0500

    changed code a bit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return None
     length = len(sentence)
     if length > 0:
-        tuple_lf = (length, sentence[0])
-        return tuple_lf
+        return (length, sentence[0])
+    else:
+        return None
