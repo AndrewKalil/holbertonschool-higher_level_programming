08b64ca155be5ed6c36aa3f10d7df1f4a35cfea1
AndrewKalil <1541@holbertonschool.com>
2020-05-05 12:37:53 -0500

    removed loop

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    for i in range(len(my_list)):
-        if idx > len(my_list):
-            return my_list
-        elif i == idx:
-            my_list[i] = element
-            return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
