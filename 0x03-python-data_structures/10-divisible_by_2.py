28284ed1eb53ed248d2ba11b6f165f57bdc134a2
AndrewKalil <1541@holbertonschool.com>
2020-05-05 16:26:39 -0500

    adding empty list condition

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new = []
-    for i in my_list:
-        if my_list[i] % 2 == 0:
-            new.append(True)
-        elif my_list[i] % 2 != 0:
-            new.append(False)
-    return new
+    if my_list:
+        for i in my_list:
+            if my_list[i] % 2 == 0:
+                new.append(True)
+            elif my_list[i] % 2 != 0:
+                new.append(False)
+        return new
+    return None
