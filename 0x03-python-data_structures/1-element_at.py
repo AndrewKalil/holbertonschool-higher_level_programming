540c4311a5e1a43488f112eecf575a43dcea036c
AndrewKalil <1541@holbertonschool.com>
2020-05-05 11:21:04 -0500

    task 2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    for i in range(len(my_list)):
+        if i == idx:
+            return my_list[i]
+        elif i > idx:
+            return None
+    return None
