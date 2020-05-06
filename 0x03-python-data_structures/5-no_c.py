a1a41aa25faca698d3804c6cf9dbab4807e0800a
AndrewKalil <1541@holbertonschool.com>
2020-05-05 19:43:39 -0500

    fixing last check

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
-    if my_string:
-        for i in my_string:
-            if i != 'c' and i != 'C':
-                new += i
-        return new
+    new_string = ""
+    for i in list(my_string):
+        if i != 'c' and i != 'C':
+            new_string = new_string + i
+    return new_string
