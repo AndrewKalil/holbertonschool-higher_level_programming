5e96efa69ffc9679fafea90efb0461663272b604
AndrewKalil <1541@holbertonschool.com>
2020-05-07 12:43:52 -0500

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dic = {}
+    for i in a_dictionary.keys():
+        num = a_dictionary[i] * 2
+        new_dic[i] = num
+    return new_dic
