212facf83bdac51bd4cc1e69424a19322e8a90e0
AndrewKalil <1541@holbertonschool.com>
2020-05-07 11:35:06 -0500

    task two

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new = []
+    num = 0
+    for i in my_list:
+        if i not in new:
+            new.append(i)
+            num += i
+    return num
