d2fad39b3704a4f8e712441589b17bb3136f186b
AndrewKalil <1541@holbertonschool.com>
2020-05-07 15:53:36 -0500

    task 12

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -17,3 +17,24 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "DCCVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IV"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "CDXCIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "XCIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "XLIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "XLIV"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "I"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "III"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
