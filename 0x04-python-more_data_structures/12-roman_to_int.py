415fc0e78a766a5308b66e05d04ae034e93b6bff
AndrewKalil <1541@holbertonschool.com>
2020-05-07 16:26:53 -0500

    inversed condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,15 +2,15 @@
 def roman_to_int(roman_string):
     s = roman_string
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string is not None or type(roman_string) == str:
-        num = 0
-        for i in range(len(roman_string)):
-            if (i > 0 and dic[s[i]] > dic[s[i - 1]]):
-                first = dic[s[i]]
-                second = dic[s[i - 1]]
-                num += first - second * 2
-            else:
-                first = dic[s[i]]
-                num += first
-        return num
-    return 0
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    num = 0
+    for i in range(len(s)):
+        if (i > 0 and dic[s[i]] > dic[s[i - 1]]):
+            first = dic[s[i]]
+            second = dic[s[i - 1]]
+            num += first - second * 2
+        else:
+            first = dic[s[i]]
+            num += first
+    return num
