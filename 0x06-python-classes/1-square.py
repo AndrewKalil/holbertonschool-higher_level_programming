161c3e6e1cbc6b22fc7ea116d478be2de47fac83
AndrewKalil <1541@holbertonschool.com>
2020-05-19 13:19:24 -0500

    task 0 and 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+class Square:
+    """class Square
+    """
+    def __init__(self, size):
+        """Instantiation
+
+        Arguments:
+            size {int} -- size of square
+        """
+        self.__size = size
