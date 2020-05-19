9a22e7b9ec5b9567134102049b3cc979cc4e036b
AndrewKalil <1541@holbertonschool.com>
2020-05-19 17:02:29 -0500

    addind an else

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,22 +13,10 @@ class Square:
             position {tuple} -- position of the square (default: {(0,0)})
 
         Raises:
-            TypeError: Raises error if position is not a positive value
-            TypeError: Raises error if position is not a positive value
-            TypeError: Raises error if position is not a positive value
-            TypeError: Raises error if position is not a positive value
             TypeError: Raises error if variable size is not an integer
             ValueError: Raises error if size is out of rang or a negative
         """
-        if len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(position) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif not(type(position[0]) is int and type(position[0]) is int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(size) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
@@ -60,7 +48,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     @property
     def position(self):
@@ -92,7 +81,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value[0]) is not int and type(value[0]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """squares the size to compute the area of the square
