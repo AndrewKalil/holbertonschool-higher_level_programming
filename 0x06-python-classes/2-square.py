1c7a4760b308f5615afecdeb1d68a950327876ff
AndrewKalil <1541@holbertonschool.com>
2020-05-19 15:17:57 -0500

    checked all tasks with pep8

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+class Square:
+    """class Square
+    """
+    def __init__(self, size=0):
+        """Instantiation
+
+        Keyword Arguments:
+            size {int} -- size of the square (default: {0})
+
+        Raises:
+            TypeError: Raises error if type of var size in not int
+            ValueError: Raises error if value if is out of range
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
