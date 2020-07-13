8a13c76bda2cc63f163b0163f69e4e00ad60aff4
AndrewKalil <1541@holbertonschool.com>
2020-07-13 13:37:48 -0500

    adding tasks 6-9

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- insert values into a table
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- counts the amount of rows containing id number 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
