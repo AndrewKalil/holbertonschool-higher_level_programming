8c8a34322f41587e73918d1ffaeb2bc67e262c81
AndrewKalil <1541@holbertonschool.com>
2020-07-14 15:47:34 -0500

    13 and 14

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,2 @@
--- list genres and number of shoes for each genre
-SELECT tv_genres.name
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows' FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
