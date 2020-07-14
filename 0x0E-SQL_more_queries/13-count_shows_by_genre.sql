4cd5a5e78a8b90b992f001c5ea937c14e2e69a0a
AndrewKalil <1541@holbertonschool.com>
2020-07-14 15:41:32 -0500

    fixing 9 and 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,2 @@
 -- list genres and number of shoes for each genre
-SELECT name
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-GROUP BY name
-ORDER BY name ASC;
+SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id WHERE tv_shows.title='Dexter' ORDER BY tv_genres.name ASC;
