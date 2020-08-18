6814f4345b10c27845391d73775fad73173c86c9
AndrewKalil <1541@holbertonschool.com>
2020-08-18 14:18:05 -0500

    fixing task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""  """
+""" find the peak of a list """
 
 
 def find_peak(list_of_integers):
-    if list_of_integers == []:
-        return
+    """ find the peak of a list"""
+    if list_of_integers == [] or list_of_integers is None:
+        return None
     max = 0
     for i in list_of_integers:
         if i > max:
