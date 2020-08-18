b6e1c69e0551f76fe53105d10760d035d5acbac9
AndrewKalil <1541@holbertonschool.com>
2020-08-18 12:02:17 -0500

    shellchecK

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-curl -sI $1 | grep "Content-Length" | tr " " "\n" | head -n2 | tail -n1 
+curl -sI "$1" | grep "Content-Length" | tr " " "\n" | head -n2 | tail -n1 
