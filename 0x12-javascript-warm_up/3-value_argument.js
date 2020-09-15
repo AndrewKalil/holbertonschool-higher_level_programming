703f0e99852ea55c103e46163f990bc3f6962ad6
AndrewKalil <1541@holbertonschool.com>
2020-09-15 15:10:48 -0500

    all up to 7 checked and all

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// prints value of arguements
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// converts a number to an integer
+
+if (process.argv.length === 2 || isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
