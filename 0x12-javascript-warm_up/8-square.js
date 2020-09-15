730063188201dc599db91ec7ce0b47fe61d96775
AndrewKalil <1541@holbertonschool.com>
2020-09-15 15:29:47 -0500

    add function

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+if (process.argv.length === 2 || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const x = parseInt(process.argv[2]);
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+}
