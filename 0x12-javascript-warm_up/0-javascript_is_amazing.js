badfe2f0f2aab5bec6353f89bc8346cf899d3af0
AndrewKalil <1541@holbertonschool.com>
2020-09-15 13:52:59 -0500

    first task

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// prints “Javascript is amazing"
+
+// declaration of variable and value assignment
+const myVar = 'Javascript is amazing';
+// print value of variable
+console.log(myVar);
