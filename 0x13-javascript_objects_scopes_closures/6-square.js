0d0ab3cbf3cd253335b3db8280938e9cbb47610a
AndrewKalil <1541@holbertonschool.com>
2020-09-21 14:19:22 -0500

    found error

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ class Square extends square {
       super.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }
